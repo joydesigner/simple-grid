512b2983c1bbbf155274fdbee0796e5c549d2ca6
joydesigner <jason.zhengxin@gmail.com>
2015-10-19 22:21:21 +1100

    updated to the percentage width

---
+++ fluid.js
@@ -0,0 +1,10 @@
+/**
+ * Created by xinzheng on 19/10/15.
+ */
+function fluidGrid() {
+    var container = document.getElementsByClassName('grid-container'),
+        sqareGrid = document.getElementsByClassName('square-grid'),
+        containerHeight = container.offsetHeight;
+
+
+}
